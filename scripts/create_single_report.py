6eae4546650c051287c40d471469d79913bc6ce6
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2021-06-08 13:36:14 +0300

    Fix script.

--- scripts/create_single_report.py
+++ scripts/create_single_report.py
@@ -3,9 +3,9 @@
 from Bio import SeqIO
         
 contig_count = 0
-with open(input.assembly, 'r') as infile:
+with open(snakemake.input.assembly, 'r') as infile:
     for record in SeqIO.parse(infile, 'fasta'):
         contig_count += 1
 
-with open(output.report, 'w') as outfile:
+with open(snakemake.output.report, 'w') as outfile:
     outfile.write('contig_count: {}\n'.format(contig_count))
